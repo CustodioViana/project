8749186b026a889830d30f65956fe5a56bd1d6fc
custodioviana <custodioviana@sapo.pt>
2023-03-16 21:43:19 +0000

    Adicionado opção de random

--- gera_validar_cpf.py
+++ gera_validar_cpf.py
@@ -1,19 +1,22 @@
 import os
 import re
 import sys
+import random
 
 sair = False
+
 while True:
     print('-')
     try:
-        escolha = int(input('Você deseja gerar ou Validar um CPF?\n'
-                            '[1] Gerar CPF\n'
-                            '[2] Validar CPF\n'
-                            '[3] Sair\n'
+        escolha = int(input('Você deseja Gerar ou Validar um CPF?\n'
+                            '[1] Gerar 1 CPF\n'
+                            '[2] Gerar 100 CPFs\n'
+                            '[3] Validar CPF\n'
+                            '[4] Sair\n'
                             ': '))
 
         print('-')
-        if escolha > 3:
+        if escolha > 4:
             print('Escolha entre as opções 1, 2 ou 3')
 
         elif escolha == 1:
@@ -54,6 +57,32 @@ while True:
             print(f'Esse é o seu CPF gerado {cpf_gerado}')
 
         elif escolha == 2:
+            for _ in range(100):
+                cpf_gerar = ''
+                for i in range(9):
+                    cpf_gerar += str(random.randint(0, 9))
+
+                dig_9 = cpf_gerar[:9]
+                cr_1 = 10
+                resultado_1 = 0
+                for digito in dig_9:
+                    resultado_1 += int(digito) * cr_1
+                    cr_1 -= 1
+                dig_1 = (resultado_1 * 10) % 11
+
+                dig_10 = dig_9 + str(dig_1)
+                cr_2 = 11
+                resutado_2 = 0
+                for digito in dig_10:
+                    resutado_2 += int(digito) * cr_2
+                    cr_2 -= 1
+                dig_2 = (resutado_2 * 10) % 11
+
+                cpf_gerado = f'{dig_9}{dig_1}{dig_2}'
+
+                print(f'{cpf_gerado}')
+
+        elif escolha == 3:
             cpf = input('Digite o CPF para Validação: ')
             print('-')
             cpf = re.sub(
@@ -89,12 +118,11 @@ while True:
                 print(f'Este CPF {cpf} é Válido')
             else:
                 print('CPF inválido')
-        elif escolha == 3:
-            os.system('cls')        
-            print('Obrigado por usar meu sistema de CPF')            
+        elif escolha == 4:
+            os.system('cls')
+            print('Obrigado por usar meu sistema de CPF')
             break
     except TypeError:
         print('Letras não são permitidas. Tente novamente')
     except ValueError:
         print('Letras não são permitidas. Tente novamente')
-
